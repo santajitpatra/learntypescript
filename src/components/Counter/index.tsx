87c62d4ec18a04b2b44ba63b5f096a6c00342546
Santajit Patra <business.santajitpatra@gmail.com>
2023-08-19 00:12:46 +0530

    fix : add number type
    add number type

--- src/components/Counter/index.tsx
+++ src/components/Counter/index.tsx
@@ -1,24 +1,33 @@
-import React, { useState } from 'react'
-import "./style.css"
+import React, { useState } from "react";
+import "./style.css";
 const Counter: React.FC = (props) => {
-const [counter, setCounter] = useState(0);
+  const [counter, setCounter] = useState<number>(0);
 
-const handleIncrement =() => {
-setCounter(counter +1)
-}
-const handleDecrement = () => {
-if (counter === 0) {
-    setCounter(counter - 1);
-}
-};
+  const handleIncrement = () => {
+    setCounter(counter + 1);
+  };
+  const handleDecrement = () => {
+    if (counter === 0) return;
+     setCounter(counter - 1);
+  };
 
   return (
     <div className="counter_container">
       <h2>{counter}</h2>
-      <button onClick={handleIncrement}>INC</button>
-      <button onClick={handleDecrement}>DEC</button>
+      <button
+        onClick={handleIncrement}
+        style={{ backgroundColor: "lightseagreen" }}
+      >
+        INC
+      </button>
+      <button
+        onClick={handleDecrement}
+        style={{ backgroundColor: "tomato" }}
+      >
+        DEC
+      </button>
     </div>
   );
-}
+};
 
-export default Counter
+export default Counter;
